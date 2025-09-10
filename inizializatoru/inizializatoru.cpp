cb7283655b55887539adbde0fc60da274900dc06
Рогачевский Иван <ivanrogacevskiy7@gmail.com>
2025-09-10 14:47:36 +0300

    на паре

--- inizializatoru/inizializatoru.cpp
+++ inizializatoru/inizializatoru.cpp
@@ -1,72 +1,136 @@
+//#include<iostream>
+//using namespace std;
+//
+//class CPU
+//{
+//	double price;
+//	double power;
+//	char* model;
+//public:
+//	CPU(): model(nullptr),power(0),price(0)
+//	{}
+//	CPU(const char* m, double pow, double pr):power(pow),price(pr)
+//	{
+//		model = new char[strlen(m) + 1];
+//		strcpy_s(model, strlen(m) + 1, m);
+//	}
+//	~CPU()
+//	{
+//		delete[] model;
+//	}
+//	void print()
+//	{
+//		cout << "Model:  " << model << endl;
+//		cout << "Power:  " << power << endl;
+//		cout << "Price:  " << price << endl;
+//	}
+//};
+//
+//class PC
+//{
+//	char* model;
+//	double price;
+//	CPU cpu;
+//public:
+//	PC(const char* mod, double p, const char* cp, double powcpu, double prcpu) :price(p), cpu(cp, powcpu, prcpu)
+//	{
+//		model = new char[strlen(mod) + 1];
+//		strcpy_s(model, strlen(mod) + 1, mod);
+//		price = p;
+//	}
+//	void Print()
+//	{
+//		cout << "-- PC:\n";
+//		cout << model << "\t" << price << endl;
+//		cpu.print();
+//	}
+//};
+//int main()
+//{
+//	PC obj("HP", 28000, "AMD razer", 3.5, 2000);
+//	obj.Print();
+//}
 #include<iostream>
-#include<time.h>
 using namespace std;
-
-class Test
+class Elevator
 {
-	const int x;
-	int temp;
-	int temp2;
+	int currentfloor;
+	int kuda;
+	bool isWorking;
 public:
-	/*Test()
+	Elevator()
 	{
-		temp = 0;
-	}*/
-	Test(int t, int t2, const int x)
-	{
-		x = 10;
-		temp = t;
-		temp2 = t2;
+		currentfloor = 1;
+		isWorking = true;
 	}
-	void Show()
+	void diapazon()
 	{
-		cout << temp << endl;
+		cout << "You are currently on the floor " << currentfloor << endl;
+		cout << "You can go to the second floor.If you want turn 2" << endl;
+		cin >> kuda;
+		if (kuda == 2)
+		{
+			moveTo(2);
+		}
+		else
+		{
+			moveTo(1);
+		}
 	}
-	int Gettemp()
+	void moveTo(int floor) // 5
 	{
-		return temp;
+		if (floor == 1 || floor == 2)
+		{
+			if (floor == currentfloor)
+			{
+				cout << "Error! You are already on this floor. " << endl;
+			}
+			else
+			{
+				cout << "The movement begins at " << floor << " etazh" << endl;
+				currentfloor = floor;
+			}
+		}
 	}
+	void currentFloor()
+	{
+		cout << "The current floor is equal to " << this->currentfloor << endl;
+	}
+	void turnOn() // 2
+	{
+		isWorking = true;
+		cout << "Lift is turn on and ready to work!" << endl;
+	}
+	void turnOff()
+	{
+		isWorking = false;
+		cout << "Lift is turn off" << endl;
+	}
+	bool getStatus()
+	{
+		return isWorking;
+	}
+	void setStatus(bool status)
+	{
+		isWorking = status;
+	}
+
 };
-class Point
-{
-	const int x;
-	int y;
-	int& z;
-	Test obj;
-public:
-
-	Point(int x1, int y1, int z1, int t1, int t2) :x(x1), z(z1), obj(t1, t2) // инициализаторы
-	{
-		cout << "Construct by 4 param\n";
-		y = y1;
-
-	}
-	void Init()
-	{
-		//	x = rand() % 10;
-		y = rand() % 10;
-	}
-	void Output()const
-	{
-		cout << "X: " << x << "\tY: " << y << endl;
-	}
-	int Getobj()
-	{
-		return obj.Gettemp();
-	}
-};
-
 int main()
 {
-	srand(unsigned(time(0)));
-	Point a(10, 1, 100, 1000, 8);
-	Point b(20, 2, 200, 2000, 9);
-	cout << b.Getobj() << endl;
-
-
-
-	a.Output();
-
-
-	system("pause");
+	
+	Elevator lift;
+	if (lift.getStatus())
+	{
+		cout << "Status : Lift is working" << endl;
+	}
+	else
+	{
+		cout << "Status : Lift isnt working" << endl;
+	}
+	lift.turnOn();
+	lift.diapazon();
+	lift.getStatus();
+	lift.currentFloor();
+	lift.turnOff();
 }
